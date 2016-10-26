033bfb959bf50faeba1b48b4485faca5c7fadb1c
海琪网络科技 <hqwlkj@outlook.com>
2016-10-26 17:35:12 +0800

    初始化项目构建

--- src/config/dev.js
+++ src/config/dev.js
@@ -3,7 +3,8 @@
 import baseConfig from './base';
 
 let config = {
-  appEnv: 'dev'  // feel free to remove the appEnv property here
+  appEnv: 'dev',  // feel free to remove the appEnv property here
+  host:'http://120.76.212.104:8081'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
